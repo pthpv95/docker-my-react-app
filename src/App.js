8aa76a2a92dccfe4c283a50555d494428e564311
Hien Pham <thehienpv@gmail.com>
2018-12-01 23:01:13 +0700

    Add some Hooks and socket.io

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ReactHookExample } from "./components/Hooks";
+import socketIOClient from "socket.io-client";
+const socket = socketIOClient('http://localhost:3000')
 
 class App extends Component {
+  state = {};
+  componentDidMount() {
+    socket.on("connect", () => {
+      console.log("connect to server success");
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ReactHookExample />
       </div>
     );
   }
---
+++ src/components/Hooks/index.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+import logo from "../../logo.svg";
+function Example() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("update");
+
+    document.title = `You click ${count} times`;
+  });
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+
+function Mouse(props) {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  return (
+    <div
+      style={{ height: "100%" }}
+      onMouseMove={e =>
+        setCoords({
+          x: e.clientX,
+          y: e.clientY
+        })
+      }
+    >
+      {props.render(coords)}
+    </div>
+  );
+}
+
+function Shadow({ mouse }) {
+  return (
+    <img
+      src={logo}
+      width='30px'
+      style={{ position: "absolute", left: mouse.x, top: mouse.y }}
+    />
+  );
+}
+
+function MouseTracker() {
+  return (
+    <div>
+      <h1>Move the mouse around!</h1>
+      <Mouse render={mouse => <Shadow mouse={mouse} />} />
+    </div>
+  );
+}
+
+export const ReactHookExample = () => {
+  return (
+    <div>
+      {/* <Example />; */}
+      <MouseTracker />
+    </div>
+  );
+};
